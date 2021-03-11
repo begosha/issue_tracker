e9898701da011554fb527fa21738e86dc51ff76f
Gosha <begoshaaaa@gmail.com>
2021-03-11 17:44:59 +0600

    index view is created

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Task, Status, Type
+from django.urls import reverse
+from django.views.generic import View, TemplateView, RedirectView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+   template_name = 'index.html'
+   
+   def get_context_data(self, **kwargs):
+       kwargs['tasks'] = Task.objects.all()
+       return super().get_context_data(**kwargs)
+
