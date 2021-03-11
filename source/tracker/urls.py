477b638c2313c24b12b0b9ae620986fc610432ef
Gosha <begoshaaaa@gmail.com>
2021-03-11 18:32:00 +0600

    path to task_add_view is added

--- source/tracker/urls.py
+++ source/tracker/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from webapp.views import (IndexView, TaskView)
+from webapp.views import (IndexView, TaskView, TaskAddView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
-    path('<int:pk>/', TaskView.as_view(), name='task')
+    path('<int:pk>/', TaskView.as_view(), name='task'),
+    path('add/', TaskAddView.as_view(), name='task_add')
 ]
