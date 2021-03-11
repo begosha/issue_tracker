789d2bc53c3d1822d11968db7270ed00fecc49c9
Gosha <begoshaaaa@gmail.com>
2021-03-11 18:33:47 +0600

    task add view is created

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from webapp.models import Task, Status, Type
+from webapp.forms import TaskForm
 from django.urls import reverse
 from django.views.generic import View, TemplateView, RedirectView
 
@@ -19,4 +20,25 @@ class TaskView(TemplateView):
     def get_context_data(self, **kwargs):
         kwargs['task'] = get_object_or_404(Task, id=kwargs.get('pk'))
         return super().get_context_data(**kwargs)
+class TaskAddView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = TaskForm()
+        return render(request, 'task_add_view.html', context={'form': form})
+       
+    def post(self, request, *args, **kwargs):
+        form = TaskForm(data=request.POST)  
+        if form.is_valid():  
+            task = Task.objects.create(
+                summary=form.cleaned_data.get('summary'),
+                description=form.cleaned_data.get('description'),
+                status=form.cleaned_data.get('status'),
+                task_type=form.cleaned_data.get('task_type')
+            )
+            return redirect('task', pk=task.id)  
+        return render(request, 'task_add_view.html', context={'form': form}) 
+       
+       
+       
+            
 
