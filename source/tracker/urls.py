943d43bb38a59ca86d4861246a734d5c747cd9d5
Gosha <begoshaaaa@gmail.com>
2021-03-12 23:44:12 +0600

    paths to update and delte views are added

--- source/tracker/urls.py
+++ source/tracker/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from webapp.views import (IndexView, TaskView, TaskAddView)
+from webapp.views import (IndexView, TaskView, TaskAddView, UpdateView, DeleteView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('<int:pk>/', TaskView.as_view(), name='task'),
-    path('add/', TaskAddView.as_view(), name='task_add')
+    path('add/', TaskAddView.as_view(), name='task_add'),
+    path('<int:pk>/update', UpdateView.as_view(), name='task-update'),
+    path('<int:pk>/delete', DeleteView.as_view(), name='task-delete')
 ]
