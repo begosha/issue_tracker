7324071da2144e402764aab65fdb41ff19419f0d
Gosha <begoshaaaa@gmail.com>
2021-03-12 23:45:27 +0600

    update and delete views are added

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -20,6 +20,7 @@ class TaskView(TemplateView):
     def get_context_data(self, **kwargs):
         kwargs['task'] = get_object_or_404(Task, id=kwargs.get('pk'))
         return super().get_context_data(**kwargs)
+
 class TaskAddView(View):
 
     def get(self, request, *args, **kwargs):
@@ -37,8 +38,39 @@ class TaskAddView(View):
             )
             return redirect('task', pk=task.id)  
         return render(request, 'task_add_view.html', context={'form': form}) 
+
+class UpdateView(View):
+    
+    def get(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, id=kwargs.get('pk'))
+        print(task)
+        form = TaskForm(initial={ 
+            'summary': task.summary,
+            'description': task.description,
+            'status': task.status,
+            'task_type': task.task_type
+        })  
+        return render(request, 'task_update_view.html', context={'form': form, 'task': task})  
+    
+    def post(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, id=kwargs.get('pk'))
+        form = TaskForm(data=request.POST)
+        if form.is_valid(): 
+            task.summary=form.cleaned_data.get('summary'),
+            task.description=form.cleaned_data.get('description'),
+            task.status=form.cleaned_data.get('status'),
+            task.task_type=form.cleaned_data.get('task_type')
+            return redirect('task', pk=task.id)   
+
+        return render(request, 'task_update_view.html', context={'form': form, 'task': task}) 
+
        
-       
-       
-            
+class DeleteView(TemplateView):
+   template_name = 'index.html'
+   
+   def get_context_data(self, **kwargs):
+       task = get_object_or_404(Task, id=kwargs.get('pk'))
+       task.delete()
+       kwargs['tasks'] = Task.objects.all()
+       return super().get_context_data(**kwargs)
 
