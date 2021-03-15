eadf8167b4cb72a29925643bcdb55e3c7e3b56f2
Gosha <begoshaaaa@gmail.com>
2021-03-15 15:04:06 +0600

    related_name of old status and type fields are edited && new fields for many to many relation are added

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -12,8 +12,10 @@ class Task(BaseModel):
 
     summary = models.CharField(max_length=300, null=False, blank=False, verbose_name='Title')
     description = models.TextField(max_length=3000, verbose_name='Description')
-    status = models.ForeignKey('webapp.Status', related_name='statuses', on_delete=models.PROTECT, verbose_name='Status', null=False,blank=False) 
-    task_type = models.ForeignKey('webapp.Type', related_name='types', on_delete=models.PROTECT, verbose_name='Type', null=False,blank=False) 
+    status = models.ForeignKey('webapp.Status', related_name='task_statuses', on_delete=models.PROTECT, verbose_name='Status', null=False,blank=False) 
+    task_type = models.ForeignKey('webapp.Type', related_name='task_types', on_delete=models.PROTECT, verbose_name='Type', null=False,blank=False) 
+    status_new = models.ManyToManyField('webapp.Status', related_name='statuses', verbose_name='Status', null=False,blank=False) 
+    task_type_new = models.ManyToManyField('webapp.Type', related_name='types', verbose_name='Type', null=False,blank=False) 
     
     class Meta:
         db_table = 'tasks'
@@ -45,3 +47,4 @@ class Type(BaseModel):
         verbose_name_plural = 'Types'
     def __str__(self):
         return "{}".format(self.task_type)
+
