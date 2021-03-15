a674f8848e2748ca4d048d89ad5ed9529aa37a5a
Gosha <begoshaaaa@gmail.com>
2021-03-15 17:07:42 +0600

    model choice is edited to modelmultiplechoice

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -6,7 +6,7 @@ from webapp.models import Task, Type, Status
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        task_type = forms.ModelChoiceField(queryset=Type.objects.all())
+        task_type = forms.ModelMultipleChoiceField(required=False, label='Types',queryset=Type.objects.all())
         status = forms.ModelChoiceField(queryset=Status.objects.all())
         fields = ('summary','description', 'status', 'task_type')
 
