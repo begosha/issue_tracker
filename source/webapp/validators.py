1831b1ff4903b50ab5d4bf8f60d86d81b7d4d9e7
Gosha <begoshaaaa@gmail.com>
2021-03-18 17:58:56 +0600

    maxlength validator is added

--- source/webapp/validators.py
+++ source/webapp/validators.py
@@ -12,3 +12,14 @@ class MinLengthValidator(BaseValidator):
 
     def clean(self, x):
         return len(x)
+
+class MaxLengthValidator(BaseValidator):
+    message = 'Value "%(value)s" has length of %(show_value)d! It should not exceed %(limit_value)d symbols!'
+    code = 'too_long'
+
+    def compare(self, a, b):
+        return a > b
+
+    def clean(self, x):
+        return len(x)
+
