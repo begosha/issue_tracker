3418f8af72bcc6b26b343c365df6b7d965a3ab87
Gosha <begoshaaaa@gmail.com>
2021-03-22 20:30:42 +0600

    new methods for index view are added for search form

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from webapp.models import Task, Status, Type
-from webapp.forms import TaskForm
+from webapp.forms import TaskForm, SimpleSearchForm
 from django.urls import reverse
 from django.views.generic import View, TemplateView, RedirectView, FormView, ListView
 from .base_view import CustomFormView
+from django.db.models import Q
+from django.utils.http import urlencode
 
 
 class IndexView(ListView):
@@ -14,9 +16,33 @@ class IndexView(ListView):
    paginate_by = 10
    paginate_orphans = 1
    
-#    def get_context_data(self, **kwargs):
-#        kwargs['tasks'] = Task.objects.all()
-#        return super().get_context_data(**kwargs)
+   def get(self, request, *args, **kwargs):
+       self.form = self.get_search_form()
+       self.search_value = self.get_search_value()
+       return super().get(request, *args, **kwargs)
+
+   def get_context_data(self, *, object_list=None, **kwargs):
+       context = super().get_context_data(object_list=object_list, **kwargs)
+       context['form'] = self.form
+       if self.search_value:
+           context['query'] = urlencode({'search': self.search_value})
+       return context
+
+   def get_queryset(self):
+       queryset = super().get_queryset()
+       if self.search_value:
+           query = Q(summary__icontains=self.search_value) | Q(description__icontains=self.search_value)
+           queryset = queryset.filter(query)
+       return queryset
+
+   def get_search_form(self):
+       return SimpleSearchForm(self.request.GET)
+       
+   def get_search_value(self):
+       if self.form.is_valid():
+           return self.form.cleaned_data['search']
+       return None
+  
 
 
 class TaskView(TemplateView):
