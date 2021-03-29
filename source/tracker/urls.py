ac889ad905a073133dbddcbaa01805b9ec6d35fb
Gosha <begoshaaaa@gmail.com>
2021-03-29 14:49:22 +0600

    urls is pieced

--- source/tracker/urls.py
+++ source/tracker/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from webapp.views import (IndexView, TaskView, TaskAddView, UpdateView, DeleteView)
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+
+HOMEPAGE_URL = 'tasks/'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
-    path('<int:pk>/', TaskView.as_view(), name='task'),
-    path('add/', TaskAddView.as_view(), name='task_add'),
-    path('<int:pk>/update', UpdateView.as_view(), name='task-update'),
-    path('<int:pk>/delete', DeleteView.as_view(), name='task-delete')
+    path('tasks/', include('webapp.urls')),
+    path('', RedirectView.as_view(url=HOMEPAGE_URL, permanent=False))
 ]
