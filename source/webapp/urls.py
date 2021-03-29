c98dc779bfd2a1e5a38ae24c7aa81bf7a60487bc
Gosha <begoshaaaa@gmail.com>
2021-03-29 18:05:41 +0600

    paths to delete and cfreate are added

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views.task_views import (IndexView, TaskView, TaskAddView, UpdateView, DeleteView)
-from .views.project_views import ProjectView
+from .views import (TaskView, UpdateView, DeleteView, IndexView, ProjectView, TaskCreate, ProjectCreate, DeleteProject)
+
 urlpatterns = [
-    path('', ProjectView.as_view(), name='index'),
-    path('<int:pk>/', TaskView.as_view(), name='task'),
-    path('add/', TaskAddView.as_view(), name='task_add'),
+    path('', IndexView.as_view(), name='index'),
+    path('<int:pk>/', ProjectView.as_view(), name='project'),
+    path('<int:pk>/task', TaskView.as_view(), name='task'),
+    path('add/', ProjectCreate.as_view(), name='project-add'),
+    # path('<int:pk>/update', UpdateView.as_view(), name='project-update'),
+    path('<int:pk>/add/', TaskCreate.as_view(), name='task_add'),
     path('<int:pk>/update', UpdateView.as_view(), name='task-update'),
-    path('<int:pk>/delete', DeleteView.as_view(), name='task-delete')
+    path('<int:pk>/delete', DeleteView.as_view(), name='task-delete'),
+    path('<int:pk>/delete/project', DeleteProject.as_view(), name='project-delete'),
+
 ]
