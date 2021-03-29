f2e30e1485bb5e5d66b0695e76051a9344b2a0a1
Gosha <begoshaaaa@gmail.com>
2021-03-29 16:36:45 +0600

    create path to project list view

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views.task_views import (IndexView, TaskView, TaskAddView, UpdateView, DeleteView)
+from .views.project_views import ProjectView
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', ProjectView.as_view(), name='index'),
     path('<int:pk>/', TaskView.as_view(), name='task'),
     path('add/', TaskAddView.as_view(), name='task_add'),
     path('<int:pk>/update', UpdateView.as_view(), name='task-update'),
