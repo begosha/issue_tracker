6dc7dd642ab2b4ad61c38eb792b6b40d55ccbcc2
Gosha <begoshaaaa@gmail.com>
2021-03-29 18:29:30 +0600

    path to project update added

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import (TaskView, UpdateView, DeleteView, IndexView, ProjectView, TaskCreate, ProjectCreate, DeleteProject)
+from .views import (TaskView, UpdateView, DeleteView, IndexView, ProjectView, TaskCreate, ProjectCreate, DeleteProject, ProjectUpdate)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('<int:pk>/', ProjectView.as_view(), name='project'),
     path('<int:pk>/task', TaskView.as_view(), name='task'),
     path('add/', ProjectCreate.as_view(), name='project-add'),
-    # path('<int:pk>/update', UpdateView.as_view(), name='project-update'),
+    path('<int:pk>/update/project', ProjectUpdate.as_view(), name='project-update'),
     path('<int:pk>/add/', TaskCreate.as_view(), name='task_add'),
     path('<int:pk>/update', UpdateView.as_view(), name='task-update'),
     path('<int:pk>/delete', DeleteView.as_view(), name='task-delete'),
