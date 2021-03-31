39e12069ab116506fd57407f060a78f251b2e9c0
Gosha <begoshaaaa@gmail.com>
2021-03-31 17:59:24 +0600

    paths to delete and update are updated

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,17 +1,26 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import (TaskView, UpdateView, DeleteView, IndexView, ProjectView, TaskCreate, ProjectCreate, DeleteProject, ProjectUpdate)
+from .views import (TaskView,
+                    TaskUpdateView,
+                    TaskDeleteView,
+                    IndexView,
+                    ProjectView,
+                    TaskCreate,
+                    ProjectCreate,
+                    ProjectDeleteView,
+                    ProjectUpdateView
+                    )
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('<int:pk>/', ProjectView.as_view(), name='project'),
     path('<int:pk>/task', TaskView.as_view(), name='task'),
     path('add/', ProjectCreate.as_view(), name='project-add'),
-    path('<int:pk>/update/project', ProjectUpdate.as_view(), name='project-update'),
+    path('<int:pk>/update/project', ProjectUpdateView.as_view(), name='project-update'),
     path('<int:pk>/add/', TaskCreate.as_view(), name='task_add'),
-    path('<int:pk>/update', UpdateView.as_view(), name='task-update'),
-    path('<int:pk>/delete', DeleteView.as_view(), name='task-delete'),
-    path('<int:pk>/delete/project', DeleteProject.as_view(), name='project-delete'),
+    path('<int:pk>/update', TaskUpdateView.as_view(), name='task-update'),
+    path('<int:pk>/delete', TaskDeleteView.as_view(), name='task-delete'),
+    path('<int:pk>/delete/project', ProjectDeleteView.as_view(), name='project-delete'),
 
 ]
