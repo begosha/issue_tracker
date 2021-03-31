93c53922e6175b9d6bdf0e2ae3494801c076d8e1
Gosha <begoshaaaa@gmail.com>
2021-03-31 18:00:00 +0600

    update and delete views are modified

--- source/webapp/views/project_views.py
+++ source/webapp/views/project_views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from ..models import Task, Status, Type, Project
 from ..forms import TaskForm, SimpleSearchForm, ProjectForm
-from django.urls import reverse
-from django.views.generic import View, TemplateView, RedirectView, FormView, ListView, DetailView, CreateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import View, TemplateView, RedirectView, FormView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from ..base_view import CustomFormView
 from django.db.models import Q
 from django.utils.http import urlencode
@@ -79,46 +79,61 @@ class TaskCreate(CreateView):
         form.instance.project = project
         return super().form_valid(form)
 
-class ProjectUpdate(FormView):
+class ProjectUpdateView(UpdateView):
     form_class = ProjectForm
+    model = Project
     template_name = 'project/project_update_view.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        self.project = self.get_object()
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_initial(self):
-        return super().get_initial()
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['instance'] = self.project
-        return kwargs
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['project'] = self.project
-        return context
-
-    def get_object(self):
-        project = get_object_or_404(
-            Project, id=self.kwargs.get('pk')
-            )
-        return project
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+    context_object_name = 'project'
 
     def get_success_url(self):
         return reverse('project', kwargs={'pk': self.kwargs.get('pk')})
 
-class DeleteProject(TemplateView):
-    template_name = 'project/index.html'
 
-    def get_context_data(self, **kwargs):
-        project = get_object_or_404(Project, id=kwargs.get('pk'))
-        project.delete()
-        kwargs['projects'] = Project.objects.all()
-        return super().get_context_data(**kwargs)
-
+class ProjectDeleteView(DeleteView):
+    model = Project
+    template_name = 'project/delete.html'
+    context_object_name = 'project'
+    success_url = reverse_lazy('index')
+# class ProjectUpdate(FormView):
+#     form_class = ProjectForm
+#     template_name = 'project/project_update_view.html'
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         self.project = self.get_object()
+#         return super().dispatch(request, *args, **kwargs)
+#
+#     def get_initial(self):
+#         return super().get_initial()
+#
+#     def get_form_kwargs(self):
+#         kwargs = super().get_form_kwargs()
+#         kwargs['instance'] = self.project
+#         return kwargs
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['project'] = self.project
+#         return context
+#
+#     def get_object(self):
+#         project = get_object_or_404(
+#             Project, id=self.kwargs.get('pk')
+#             )
+#         return project
+#
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+#
+#     def get_success_url(self):
+#         return reverse('project', kwargs={'pk': self.kwargs.get('pk')})
+#
+# class DeleteProject(TemplateView):
+#     template_name = 'project/index.html'
+#
+#     def get_context_data(self, **kwargs):
+#         project = get_object_or_404(Project, id=kwargs.get('pk'))
+#         project.delete()
+#         kwargs['projects'] = Project.objects.all()
+#         return super().get_context_data(**kwargs)
+#
