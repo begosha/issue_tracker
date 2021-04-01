6681e243c7b207fbc4f131a9e91797b20e1c71bc
Gosha <begoshaaaa@gmail.com>
2021-04-01 17:58:01 +0600

    field is_deleted is added

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -25,7 +25,7 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ['id', 'project_name', 'project_description', 'start_date', 'end_date']
     list_filter = ['project_name']
     search_fields = ['project_name']
-    fields = ['id', 'project_name', 'project_description', 'start_date', 'end_date']
+    fields = ['id', 'project_name', 'project_description', 'start_date', 'end_date', 'is_deleted']
     readonly_fields = ['id']
 
 
