9c9d1fe9f985d760da143a8f7ecf8c3fff35aff9
Gosha <begoshaaaa@gmail.com>
2021-04-05 20:36:41 +0600

    logout view is added

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from accounts.views import login_view, logout_view
 
 urlpatterns = [
-    path('accounts/login', login_view, name='login'),
+    path('accounts/login/', login_view, name='login'),
     path('accounts/logout/', logout_view, name='logout')
 ]
