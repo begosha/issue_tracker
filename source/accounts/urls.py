8b7ed810e006966fef04cbe1b40f91fdd829c6ff
Gosha <begoshaaaa@gmail.com>
2021-04-05 19:25:25 +0600

    accounts app is created

---
+++ source/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from accounts.views import login_view, logout_view
+
+urlpatterns = [
+    path('accounts/login', login_view, name='login'),
+    path('accounts/logout/', logout_view, name='logout')
+]
