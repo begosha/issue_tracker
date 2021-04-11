99ab380f8e11ce1f1710e68272910ece62c1f839
Gosha <begoshaaaa@gmail.com>
2021-04-11 21:14:28 +0600

    user creation form is updated && first last name validator is added

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,25 +1,24 @@
 from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 
 
-class MyUserCreationForm(forms.ModelForm):
-    password = forms.CharField(label="Password", strip=False, required=True, widget=forms.PasswordInput)
-    password_confirm = forms.CharField(label="Confirm Password", required=True, widget=forms.PasswordInput, strip=False)
+class MyUserCreationForm(UserCreationForm):
+
     email  = forms.EmailField(required=True, widget=forms.EmailInput)
-
 
     def clean(self):
         cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password_confirm = cleaned_data.get("password_confirm")
-        if password and password_confirm and password != password_confirm:
-            raise forms.ValidationError('Passwords do not match!')
+        last_name = cleaned_data.get("last_name")
+        first_name = cleaned_data.get("first_name")
+        if last_name == '' and first_name == '':
+            raise forms.ValidationError('Fill Out First Name Field!')
 
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
+        user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
@@ -30,6 +29,7 @@ class MyUserCreationForm(forms.ModelForm):
               raise forms.ValidationError("We have a user with this user email-id")
           return data
 
+
     class Meta:
         model = User
-        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
+        fields = ['username','first_name', 'last_name', 'email']
