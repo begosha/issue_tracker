3b67889bcc93abcf105a9fb2d3956626ab4a1c47
Gosha <begoshaaaa@gmail.com>
2021-04-15 20:38:15 +0600

    path to user detail view is added

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from accounts.views import  RegisterView
+from accounts.views import  RegisterView, UserDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('accounts/login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
-    path('accounts/create/', RegisterView.as_view(), name='create')
+    path('accounts/create/', RegisterView.as_view(), name='create'),
+    path('<int:pk>/', UserDetailView.as_view(), name='detail')
 ]
