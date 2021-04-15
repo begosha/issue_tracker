f3dcde7f6ef784fa0d436116c994480fd6fa3bf7
Gosha <begoshaaaa@gmail.com>
2021-04-15 20:39:02 +0600

    user detail view  is done

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import MyUserCreationForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+    
+
+    def get_context_data(self, **kwargs):
+        projects = self.object.projects.order_by('-start_date')
+        paginator = Paginator(projects, self.paginate_related_by, orphans=self.paginate_related_orphans)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['projects'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs)
 
 class RegisterView(CreateView):
     model = User
