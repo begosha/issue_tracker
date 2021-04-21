589dfc38534fedba83b1fbe209c0e39a80f2210a
Gosha <begoshaaaa@gmail.com>
2021-04-21 21:07:16 +0600

    password change form is added

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from .models import Profile
 
 
 
-class MyUserCreationForm(UserCreationForm): 
+class MyUserCreationForm(UserCreationForm):     
     email  = forms.EmailField(required=True, widget=forms.EmailInput)
 
     def clean(self):
@@ -46,3 +48,35 @@ class ProfileChangeForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class PasswordChangeForm(forms.ModelForm):
+
+    password = forms.CharField(label="New Password", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Confirm Password", widget=forms.PasswordInput, strip=False)
+    old_password = forms.CharField(label="Old Password", strip=False, widget=forms.PasswordInput)
+
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Passwords do not match!')
+        return password_confirm
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Old password is incorrect!')
+        return old_password
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = get_user_model()
+        fields = ['password', 'password_confirm', 'old_password']
+
